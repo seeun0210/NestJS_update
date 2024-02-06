42bb25f019d6745718fb15b79625f039fdad6e79
kimseeun <cocobell3@naver.com>
2024-02-07 00:09:14 +0900

    study: Not Found Exception 던지기:

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, NotFoundException, Param } from '@nestjs/common';
 import { PostsService } from './posts.service';
+import { NotFoundError } from 'rxjs';
 interface PostModel {
   id: number;
   author: string;
@@ -46,8 +47,15 @@ export class PostsController {
   }
 
   // 2) GET /posts/:id
+  // id에 해당되는 post를 가져온다
   @Get(':id')
   getPost(@Param('id') id: string) {
-    return posts.find((post) => post.id === +id);
+    const post = posts.find((post) => post.id === +id);
+    if (!post) {
+      throw new NotFoundException();
+    }
+    return post;
   }
+  // 3)POST /posts
+  // post를 생성한다
 }
