5380261c83a324b1edb63c28e81bc77be61cdc86
kimseeun <cocobell3@naver.com>
2024-02-08 00:41:33 +0900

    study: NestJS에TypeORM 설정하기

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,27 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   //다른 모듈을 불러올 때 사용하는 imports
   //cli를 사용하면 자동으로 생성해줌!
-  imports: [PostsModule],
+  imports: [
+    PostsModule,
+    TypeOrmModule.forRoot({
+      // 데이터 베이스 타입
+      type: 'postgres',
+      host: '127.0.0.1',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      entities: [],
+      //개발환경에서는 true
+      //production한경에서는 false
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
