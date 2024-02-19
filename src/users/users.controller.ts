38e615c1eab30ad90a92e2be00c03d4008b94833
kimseeun <cocobell3@naver.com>
2024-02-19 23:31:56 +0900

    study: registerWithEmail 함수 정의하기

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -11,7 +11,7 @@ export class UsersController {
     @Body('password') password: string,
     @Body('email') email: string,
   ) {
-    return this.usersService.createUser(nickname, email, password);
+    return this.usersService.createUser({ nickname, email, password });
   }
 
   @Get()
