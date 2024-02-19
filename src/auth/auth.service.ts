a3238a597f58c4b7af636ebf5dcc7c811cdaf2cd
kimseeun <cocobell3@naver.com>
2024-02-19 22:51:33 +0900

    study: 토큰 signin

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UsersModel } from 'src/users/entities/users.entity';
+import { JWT_SECRET } from './const/auth.const';
 
 @Injectable()
 export class AuthService {
+  constructor(private readonly jwtService: JwtService) {}
   /*
    *우리가 만드려는 기능
    *
@@ -27,4 +31,23 @@ export class AuthService {
    *    3. 모두 통과하면 찾은 사용자 정보 반환
    *    4. loginWithEmail에서 반환된 데이터를 기반으로 토큰 생성
    */
+
+  /*
+   *Payload에 들어갈 정보
+   *1)email
+   *2)sub->id
+   *3)type:'access'|'refresh'
+   */
+  signToken(user: Pick<UsersModel, 'email' | 'id'>, isRefreshToken: boolean) {
+    const payload = {
+      email: user.email,
+      sub: user.id,
+      type: isRefreshToken ? 'refresh' : 'access',
+    };
+    return this.jwtService.sign(payload, {
+      secret: JWT_SECRET,
+      //seconds
+      expiresIn: isRefreshToken ? 3600 : 300,
+    });
+  }
 }
