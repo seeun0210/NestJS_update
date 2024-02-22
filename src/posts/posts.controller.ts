6052792bc16ae98dddf8ef8049e7a1c20c146c09
kimseeun <cocobell3@naver.com>
2024-02-22 16:46:08 +0900

    study: Pipe소개 & ParseIntPipe 사용해보기

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -4,6 +4,7 @@ import {
   Delete,
   Get,
   Param,
+  ParseIntPipe,
   Post,
   Put,
 } from '@nestjs/common';
@@ -22,11 +23,21 @@ export class PostsController {
 
   // 2) GET /posts/:id
   // id에 해당되는 post를 가져온다
+  // 그런데 파라미터는 무조건 string이다(url에서 추출해오기 때문)
+  // 그래서 pipe라는걸 쓴다
+  // ParseIntPipe: 숫자로 바꾸고 검증까지 해준다
+  // 만약에 숫자로 바꿀 수 없는 "adsfa"같은 값이 들어오면
+  // {
+  //     "message": "Validation failed (numeric string is expected)",
+  //     "error": "Bad Request",
+  //     "statusCode": 400
+  // }
+  //이런 에러가 뜬다
   @Get(':id')
-  getPost(@Param('id') id: string) {
+  getPost(@Param('id', ParseIntPipe) id: number) {
     //param으로 받으면 string으로 넘어옴
     //+를 붙여주면 number 가 됨
-    return this.postsService.getPostById(+id);
+    return this.postsService.getPostById(id);
   }
   // 3)POST /posts
   // post를 생성한다
@@ -42,16 +53,16 @@ export class PostsController {
   // id에 해당하는 POST를 변경한다.
   @Put(':id')
   putPost(
-    @Param('id') id: string,
+    @Param('id', ParseIntPipe) id: number,
     @Body('title') title?: string,
     @Body('content') content?: string,
   ) {
-    return this.postsService.updatePost(+id, title, content);
+    return this.postsService.updatePost(id, title, content);
   }
   // 5) DELETE /posts/:id
   // id에 해당되는 POST를 삭제한다.
   @Delete(':id')
-  deletePost(@Param('id') id: number) {
+  deletePost(@Param('id', ParseIntPipe) id: number) {
     return this.postsService.deletePost(+id);
   }
 }
