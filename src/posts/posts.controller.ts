7a6335fd56a5b918df9b57ca6bdc1f75cc370833
김세은 <seeun@gimseeun-ui-MacBookPro.local>
2025-01-19 23:09:28 +0900

    feat: transaction 시작하기

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -18,99 +18,127 @@ import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
 import { PaginatePostDto } from './dto/pagenate-post.dto';
 import { ImageModelType } from 'src/common/entity/image.entity';
+import { DataSource } from 'typeorm';
 
 @Controller('posts')
 export class PostsController {
-  constructor(private readonly postsService: PostsService) { }
+	constructor(
+		private readonly postsService: PostsService,
+		private readonly dataSource: DataSource,
+	) {}
 
-  // 1) GET /posts
-  // 모든 post를 다 가져온다
-  @Get()
-  getPosts(@Query() query: PaginatePostDto) {
-    return this.postsService.paginatePosts(query);
-  }
+	// 1) GET /posts
+	// 모든 post를 다 가져온다
+	@Get()
+	getPosts(@Query() query: PaginatePostDto) {
+		return this.postsService.paginatePosts(query);
+	}
 
-  // POST /posts/random
-  @Post('random')
-  @UseGuards(AccessTokenGuard)
-  async generatePosts(@User() user: UsersModel) {
-    await this.postsService.generatePosts(user.id);
-    return true;
-  }
+	// POST /posts/random
+	@Post('random')
+	@UseGuards(AccessTokenGuard)
+	async generatePosts(@User() user: UsersModel) {
+		await this.postsService.generatePosts(user.id);
+		return true;
+	}
 
-  // 2) GET /posts/:id
-  // id에 해당되는 post를 가져온다
-  // 그런데 파라미터는 무조건 string이다(url에서 추출해오기 때문)
-  // 그래서 pipe라는걸 쓴다
-  // ParseIntPipe: 숫자로 바꾸고 검증까지 해준다
-  // 만약에 숫자로 바꿀 수 없는 "adsfa"같은 값이 들어오면
-  // {
-  //     "message": "Validation failed (numeric string is expected)",
-  //     "error": "Bad Request",
-  //     "statusCode": 400
-  // }
-  //이런 에러가 뜬다
-  @Get(':id')
-  // @UseInterceptors(ClassSerializerInterceptor)
-  getPost(@Param('id', ParseIntPipe) id: number) {
-    //param으로 받으면 string으로 넘어옴
-    //+를 붙여주면 number 가 됨
-    return this.postsService.getPostById(id);
-  }
+	// 2) GET /posts/:id
+	// id에 해당되는 post를 가져온다
+	// 그런데 파라미터는 무조건 string이다(url에서 추출해오기 때문)
+	// 그래서 pipe라는걸 쓴다
+	// ParseIntPipe: 숫자로 바꾸고 검증까지 해준다
+	// 만약에 숫자로 바꿀 수 없는 "adsfa"같은 값이 들어오면
+	// {
+	//     "message": "Validation failed (numeric string is expected)",
+	//     "error": "Bad Request",
+	//     "statusCode": 400
+	// }
+	//이런 에러가 뜬다
+	@Get(':id')
+	// @UseInterceptors(ClassSerializerInterceptor)
+	getPost(@Param('id', ParseIntPipe) id: number) {
+		//param으로 받으면 string으로 넘어옴
+		//+를 붙여주면 number 가 됨
+		return this.postsService.getPostById(id);
+	}
 
-  // 3)POST /posts
-  // post를 생성한다
-  //
-  //DTO-Data Transfer Object(데이터 전송 객체)
-  @Post()
-  @UseGuards(AccessTokenGuard)
-  //포스트맨 또는 클라이언트에서 실제로 이미지를 업로드할 때, 이미지라는 키값에 파일을 넣어서 보내면 된다
-  async postPosts(
-    //User 커스텀데코레이터로 user정보가져오기
-    //파라미터로 UsersModel의 키값을 가져올 수 있게 해놓음
-    @User('id') userId: number,
-    @Body() body: CreatePostDto,
-    // @Body('title') title: string,
-    // @Body('content') content: string,
-    //DefaultValuePipe의 경우 new로 인스턴스화를 해줌
-    //new를 사용해서 인스턴스화를 하면 함수가 실행할때마다 계속 생김
-    //ParseIntPipe는 클래스를 그냥 그대로 입력해줌
-    //이건 NestJs의 IOC컨테이너에서 Inversion of Control컨테이너에서 자동으로 이 값을 주입해주는 것
-    //결론적으로 작동하는데는 큰 차이가 없음
-    //nestJs에서 dependency injection을 해주냐 안해주냐의 차이!
-    // @Body('isPublic', new DefaultValuePipe(true)) isPublic: boolean,
-  ) {
-    const post = await this.postsService.createPost(userId, body)
+	// 3)POST /posts
+	// post를 생성한다
+	//
+	//DTO-Data Transfer Object(데이터 전송 객체)
+	@Post()
+	@UseGuards(AccessTokenGuard)
+	//포스트맨 또는 클라이언트에서 실제로 이미지를 업로드할 때, 이미지라는 키값에 파일을 넣어서 보내면 된다
+	async postPosts(
+		//User 커스텀데코레이터로 user정보가져오기
+		//파라미터로 UsersModel의 키값을 가져올 수 있게 해놓음
+		@User('id') userId: number,
+		@Body() body: CreatePostDto,
+		// @Body('title') title: string,
+		// @Body('content') content: string,
+		//DefaultValuePipe의 경우 new로 인스턴스화를 해줌
+		//new를 사용해서 인스턴스화를 하면 함수가 실행할때마다 계속 생김
+		//ParseIntPipe는 클래스를 그냥 그대로 입력해줌
+		//이건 NestJs의 IOC컨테이너에서 Inversion of Control컨테이너에서 자동으로 이 값을 주입해주는 것
+		//결론적으로 작동하는데는 큰 차이가 없음
+		//nestJs에서 dependency injection을 해주냐 안해주냐의 차이!
+		// @Body('isPublic', new DefaultValuePipe(true)) isPublic: boolean,
+	) {
+		//트랜잭션과 관련된 모든 쿼리 러너를 담당함
+		//쿼리 러너를 생성한다.
+		const qr = this.dataSource.createQueryRunner();
 
-    for (let i = 0; i < body.images.length; i++) {
-      await this.postsService.createPostImage({
-        post,
-        order: i,
-        path: body.images[i],
-        type: ImageModelType.POST_IMAGE
-      });
-    }
+		//쿼리 러너에 연결한다.
+		await qr.connect();
+		//쿼리 러너에서 트랜잭션을 시작한다.
+		//이 시점부터 같은 쿼리 러너를 사용하면
+		//트랜잭션 안에서 데이터베이스 액션을 실행할 수 있다.
+		await qr.startTransaction();
 
-    return this.postsService.getPostById(post.id)
+		//로직 실행
+		try {
+			const post = await this.postsService.createPost(
+				userId,
+				body,
+			);
 
-  }
+			for (let i = 0; i < body.images.length; i++) {
+				await this.postsService.createPostImage({
+					post,
+					order: i,
+					path: body.images[i],
+					type: ImageModelType.POST_IMAGE,
+				});
+			}
 
-  // 4)PUT /posts/:id
-  // id에 해당하는 POST를 변경한다.
-  @Patch(':id')
-  patchPost(
-    @Param('id', ParseIntPipe) id: number,
-    @Body() body: UpdatePostDto,
-    // @Body('title') title?: string,
-    // @Body('content') content?: string,
-  ) {
-    return this.postsService.updatePost(id, body);
-  }
+			await qr.commitTransaction();
+			await qr.release();
 
-  // 5) DELETE /posts/:id
-  // id에 해당되는 POST를 삭제한다.
-  @Delete(':id')
-  deletePost(@Param('id', ParseIntPipe) id: number) {
-    return this.postsService.deletePost(+id);
-  }
+			return this.postsService.getPostById(post.id);
+		} catch (e) {
+			//어떤 에러든 에러가 던져지면
+			//트랜 잭션을 종료하고 원래 상태로 되돌린다.
+			await qr.rollbackTransaction();
+			await qr.release();
+		}
+	}
+
+	// 4)PUT /posts/:id
+	// id에 해당하는 POST를 변경한다.
+	@Patch(':id')
+	patchPost(
+		@Param('id', ParseIntPipe) id: number,
+		@Body() body: UpdatePostDto,
+		// @Body('title') title?: string,
+		// @Body('content') content?: string,
+	) {
+		return this.postsService.updatePost(id, body);
+	}
+
+	// 5) DELETE /posts/:id
+	// id에 해당되는 POST를 삭제한다.
+	@Delete(':id')
+	deletePost(@Param('id', ParseIntPipe) id: number) {
+		return this.postsService.deletePost(+id);
+	}
 }
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,13 +1,26 @@
 import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
-import { FindOptionsWhere, LessThan, MoreThan, Repository } from 'typeorm';
+import {
+	FindOptionsWhere,
+	LessThan,
+	MoreThan,
+	QueryRunner,
+	Repository,
+} from 'typeorm';
 import { PostsModel } from './entity/posts.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
 import { PaginatePostDto } from './dto/pagenate-post.dto';
-import { HOST, PORT, PROTOCOL } from 'src/common/const/env.const';
+import {
+	HOST,
+	PORT,
+	PROTOCOL,
+} from 'src/common/const/env.const';
 import { CommonService } from 'src/common/common.service';
-import { POST_IMAGE_PATH, PUBLIC_FOLDER_PATH, TEMP_FOLDER_PATH } from 'src/common/const/path.const';
+import {
+	POST_IMAGE_PATH,
+	TEMP_FOLDER_PATH,
+} from 'src/common/const/path.const';
 import { basename, join } from 'path';
 import { promises } from 'fs';
 import { CreatePostImageDto } from './image/dto/create-image.dto';
@@ -19,198 +32,229 @@ import { DEFAULT_POST_FIND_OPTIONS } from './const/default-post-find-options.con
 //1. 프로바이더로 생성하고 싶은 클래스는 모듈에다 등록해주기
 //2. @Injectable() 어노테이션 해주기
 export class PostsService {
-  constructor(
-    @InjectRepository(PostsModel)
-    private readonly postsRepository: Repository<PostsModel>,
-    @InjectRepository(ImageModel)
-    private readonly imageRepository: Repository<ImageModel>,
-    private readonly commonService: CommonService,
-  ) { }
+	constructor(
+		@InjectRepository(PostsModel)
+		private readonly postsRepository: Repository<PostsModel>,
+		@InjectRepository(ImageModel)
+		private readonly imageRepository: Repository<ImageModel>,
+		private readonly commonService: CommonService,
+	) {}
 
-  async getAllPosts() {
-    return await this.postsRepository.find({ ...DEFAULT_POST_FIND_OPTIONS });
-  }
+	async getAllPosts() {
+		return await this.postsRepository.find({
+			...DEFAULT_POST_FIND_OPTIONS,
+		});
+	}
 
-  //1) 오름차순으로 정렬하는 pagination만 구현한다.
-  async paginatePosts(dto: PaginatePostDto) {
-    return this.commonService.paginate(dto, this.postsRepository, {
-      ...DEFAULT_POST_FIND_OPTIONS,
-    }, 'posts');
-  }
+	//1) 오름차순으로 정렬하는 pagination만 구현한다.
+	async paginatePosts(dto: PaginatePostDto) {
+		return this.commonService.paginate(
+			dto,
+			this.postsRepository,
+			{
+				...DEFAULT_POST_FIND_OPTIONS,
+			},
+			'posts',
+		);
+	}
 
-  async pagePaginatePosts(dto: PaginatePostDto) {
-    if (dto.page) {
-      return this.pagePaginatePosts(dto);
-    } else {
-      return this.cursorPaginatePosts(dto);
-    }
-  }
+	async pagePaginatePosts(dto: PaginatePostDto) {
+		if (dto.page) {
+			return this.pagePaginatePosts(dto);
+		} else {
+			return this.cursorPaginatePosts(dto);
+		}
+	}
 
-  async cursorPaginatePosts(dto: PaginatePostDto) {
-    const where: FindOptionsWhere<PostsModel> = {};
+	async cursorPaginatePosts(dto: PaginatePostDto) {
+		const where: FindOptionsWhere<PostsModel> = {};
 
-    if (dto.where__id__more_than) {
-      where.id = MoreThan(dto.where__id__more_than);
-    } else if (dto.where__id__less_than) {
-      where.id = LessThan(dto.where__id__less_than);
-    }
+		if (dto.where__id__more_than) {
+			where.id = MoreThan(dto.where__id__more_than);
+		} else if (dto.where__id__less_than) {
+			where.id = LessThan(dto.where__id__less_than);
+		}
 
-    const posts = await this.postsRepository.find({
-      where,
-      order: {
-        createdAt: dto.order__createdAt,
-      },
-      take: dto.take,
-    });
-    const lastItem = posts.length > 0 && posts.length === dto.take ? posts[posts.length - 1] : null;
-    const nextUrl = lastItem && new URL(`${PROTOCOL}://${HOST}:${PORT}/posts`);
-    if (nextUrl) {
-      /**
-       * dto의 키값들을 루핑하면서 
-       * 키값에 해당되는 밸류가 존재하ㅕㄴ 
-       * param에 추가해준다.
-       * 
-       * 단, where__id_more_than 값만 lastItem의 마자믹 값으로 넣어준다
-       * 
-       */
-      for (const key of Object.keys(dto)) {
-        if (dto[key]) {
-          if (key !== 'where__id_more_than' && key !== 'where__id_less_than') {
-            nextUrl.searchParams.append(key, dto[key]);
-          }
-        }
-      }
+		const posts = await this.postsRepository.find({
+			where,
+			order: {
+				createdAt: dto.order__createdAt,
+			},
+			take: dto.take,
+		});
+		const lastItem =
+			posts.length > 0 && posts.length === dto.take
+				? posts[posts.length - 1]
+				: null;
+		const nextUrl =
+			lastItem &&
+			new URL(`${PROTOCOL}://${HOST}:${PORT}/posts`);
+		if (nextUrl) {
+			/**
+			 * dto의 키값들을 루핑하면서
+			 * 키값에 해당되는 밸류가 존재하ㅕㄴ
+			 * param에 추가해준다.
+			 *
+			 * 단, where__id_more_than 값만 lastItem의 마자믹 값으로 넣어준다
+			 *
+			 */
+			for (const key of Object.keys(dto)) {
+				if (dto[key]) {
+					if (
+						key !== 'where__id_more_than' &&
+						key !== 'where__id_less_than'
+					) {
+						nextUrl.searchParams.append(key, dto[key]);
+					}
+				}
+			}
 
-      let key = null;
+			let key = null;
 
-      if (dto.order__createdAt === 'ASC') {
-        key = 'where__id_more_than';
-      } else {
-        key = 'where__id_less_than';
-      }
+			if (dto.order__createdAt === 'ASC') {
+				key = 'where__id_more_than';
+			} else {
+				key = 'where__id_less_than';
+			}
 
-      nextUrl.searchParams.append(key, lastItem.id.toString());
-    }
-    /**
-  * 
-  * Response:
-  * {
-  *  data: Data[]
-  *  cursor:{
-  *    after: number,
-  *  },
-  *  count: 응답한 데이터의 갯수,
-  *  next: 다음 요청을 할 때 사용할 URL
-  * }
-  */
-    return {
-      data: posts,
-      cursor: {
-        after: lastItem?.id ?? null,
-      },
-      count: posts.length,
-      next: nextUrl?.toString(),
-    }
-  }
+			nextUrl.searchParams.append(
+				key,
+				lastItem.id.toString(),
+			);
+		}
+		/**
+		 *
+		 * Response:
+		 * {
+		 *  data: Data[]
+		 *  cursor:{
+		 *    after: number,
+		 *  },
+		 *  count: 응답한 데이터의 갯수,
+		 *  next: 다음 요청을 할 때 사용할 URL
+		 * }
+		 */
+		return {
+			data: posts,
+			cursor: {
+				after: lastItem?.id ?? null,
+			},
+			count: posts.length,
+			next: nextUrl?.toString(),
+		};
+	}
 
-  async generatePosts(userId: number) {
-    for (let i = 0; i < 100; i++) {
-      await this.createPost(userId, {
-        title: `임의로 생성된 post ${i}`,
-        content: `임의로 생성된 post ${i}`,
-        images: [],
-      });
-    }
-  }
+	async generatePosts(userId: number) {
+		for (let i = 0; i < 100; i++) {
+			await this.createPost(userId, {
+				title: `임의로 생성된 post ${i}`,
+				content: `임의로 생성된 post ${i}`,
+				images: [],
+			});
+		}
+	}
 
-  async getPostById(id: number) {
-    const post = await this.postsRepository.findOne({
-      ...DEFAULT_POST_FIND_OPTIONS,
-      where: { id },
-    });
-    if (!post) {
-      throw new NotFoundException();
-    }
-    return post;
-  }
+	async getPostById(id: number) {
+		const post = await this.postsRepository.findOne({
+			...DEFAULT_POST_FIND_OPTIONS,
+			where: { id },
+		});
+		if (!post) {
+			throw new NotFoundException();
+		}
+		return post;
+	}
 
-  async createPost(authorId: number, postDto: CreatePostDto) {
-    //1) create -> 저장할 객체를 생성한다.
-    //2) save -> 객체를 저장한다. (create 메서드에서 생성한 객체로)
-    const post = this.postsRepository.create({
-      author: {
-        //연동할 author의 정보를 넣어준다
-        id: authorId,
-      },
-      ...postDto,
-      images:[],
-      likeCount: 0,
-      commentCount: 0,
-    });
-    const newPost = await this.postsRepository.save(post);
+	getRepository(qr?: QueryRunner) {
+		return qr
+			? qr.manager.getRepository<PostsModel>(PostsModel)
+			: this.postsRepository;
+	}
 
-    return newPost;
-  }
+	async createPost(
+		authorId: number,
+		postDto: CreatePostDto,
+		qr?: QueryRunner,
+	) {
+		const repository = this.getRepository(qr);
+		//1) create -> 저장할 객체를 생성한다.
+		//2) save -> 객체를 저장한다. (create 메서드에서 생성한 객체로)
+		const post = this.postsRepository.create({
+			author: {
+				//연동할 author의 정보를 넣어준다
+				id: authorId,
+			},
+			...postDto,
+			images: [],
+			likeCount: 0,
+			commentCount: 0,
+		});
+		const newPost = await this.postsRepository.save(post);
 
-  async createPostImage(dto: CreatePostImageDto) {
-    //dto의 이미지 이름을 기반으로 
-    //파일의 경로를 생성한다.
-    const tempFilePath = join(
-      TEMP_FOLDER_PATH,
-      dto.path
-    )
+		return newPost;
+	}
 
-    try {
-      //access: 해당 경로의 파일에 접근이 가능한지 알려줌
-      promises.access(tempFilePath)
-    } catch (e) {
-      throw new BadRequestException('존재하지 않는 파일 입니다.')
-    }
+	async createPostImage(dto: CreatePostImageDto) {
+		//dto의 이미지 이름을 기반으로
+		//파일의 경로를 생성한다.
+		const tempFilePath = join(TEMP_FOLDER_PATH, dto.path);
 
-    //basename: 파일 이름만 추출
-    const fileName = basename(tempFilePath)
+		try {
+			//access: 해당 경로의 파일에 접근이 가능한지 알려줌
+			promises.access(tempFilePath);
+		} catch (e) {
+			throw new BadRequestException(
+				'존재하지 않는 파일 입니다.',
+			);
+		}
 
-    //새로 이동할 포스트 폴더의 경로 + 이미지 이름
-    const newPath = join(
-      POST_IMAGE_PATH,
-      fileName
-    )
-    
-    //save
-    const result = await this.imageRepository.save({...dto})
+		//basename: 파일 이름만 추출
+		const fileName = basename(tempFilePath);
 
-    //파일 옮기기
-    await promises.rename(tempFilePath, newPath)
+		//새로 이동할 포스트 폴더의 경로 + 이미지 이름
+		const newPath = join(POST_IMAGE_PATH, fileName);
 
-    return result;
-  }
+		//save
+		const result = await this.imageRepository.save({
+			...dto,
+		});
 
-  async updatePost(postId: number, postDto: UpdatePostDto) {
-    const { title, content } = postDto;
-    // save의 기능
-    // 1) 만약에 데이터가 존재하지 않는다면(id 기준으로) 새로 생성한다.
-    // 2) 만약에 데이터가 존재하다면 (같은 id의 값이 존재한다면) 존재하던 값을 업데이트한다.
-    const post = await this.postsRepository.findOne({ where: { id: postId } });
+		//파일 옮기기
+		await promises.rename(tempFilePath, newPath);
 
-    if (!post) {
-      throw new NotFoundException();
-    }
-    if (title) {
-      post.title = title;
-    }
-    if (content) {
-      post.content = content;
-    }
-    const newPost = await this.postsRepository.save(post);
-    return newPost;
-  }
+		return result;
+	}
 
-  async deletePost(postId: number) {
-    const post = await this.postsRepository.findOne({ where: { id: postId } });
-    if (!post) {
-      throw new NotFoundException();
-    }
-    await this.postsRepository.delete(postId);
-    return postId;
-  }
+	async updatePost(postId: number, postDto: UpdatePostDto) {
+		const { title, content } = postDto;
+		// save의 기능
+		// 1) 만약에 데이터가 존재하지 않는다면(id 기준으로) 새로 생성한다.
+		// 2) 만약에 데이터가 존재하다면 (같은 id의 값이 존재한다면) 존재하던 값을 업데이트한다.
+		const post = await this.postsRepository.findOne({
+			where: { id: postId },
+		});
+
+		if (!post) {
+			throw new NotFoundException();
+		}
+		if (title) {
+			post.title = title;
+		}
+		if (content) {
+			post.content = content;
+		}
+		const newPost = await this.postsRepository.save(post);
+		return newPost;
+	}
+
+	async deletePost(postId: number) {
+		const post = await this.postsRepository.findOne({
+			where: { id: postId },
+		});
+		if (!post) {
+			throw new NotFoundException();
+		}
+		await this.postsRepository.delete(postId);
+		return postId;
+	}
 }
